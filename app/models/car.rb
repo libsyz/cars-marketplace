76fb2611a677a8636224836a40b8ce7bd182b7b5
Chia Wei Ng <chiawei2410@gmail.com>
2021-05-19 17:26:36 +0800

    Added seed data

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,7 @@
 class Car < ApplicationRecord
+  has_many :car_reviews, dependent: :delete_all
+  has_many :renter_reviews, dependent: :delete_all
+  has_many :bookings, dependent: :delete_all
+  has_many :favorites, dependent: :delete_all
+  has_many :users, through: :bookings
 end
