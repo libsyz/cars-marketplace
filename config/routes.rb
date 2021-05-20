9bfc3e149724450726cfda87b76aa0cdb34de855
Chia Wei Ng <chiawei2410@gmail.com>
2021-05-20 21:58:18 +0800

    Added car details and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # resources :cars, only: [:index, :show]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
+  get "cars", to: "cars#index"
+  get "cars/:id", to: "cars#show"
   get 'favorite', to: 'pages#favorite'
 end
