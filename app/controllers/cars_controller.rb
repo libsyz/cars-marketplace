195743d630feb785b32c242d35bdf033ae763a00
Yeonsu Shin <shin.yeonsua@gmail.com>
2021-05-20 22:13:32 +0800

    home_index

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,6 @@
+class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  def index
+    @cars = Car.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'cars/index'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :cars, only: [:index]
   get "favorites/:car_id", to: "favorites#index"
 
 end
