c66d4badb2a03615687ae23f8137ba7044987dea
Chia Wei Ng <chiawei2410@gmail.com>
2021-05-21 16:12:32 +0800

    Added new car listing form

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,4 +5,12 @@ class Car < ApplicationRecord
   has_many :favorites, dependent: :delete_all
   has_many :users, through: :bookings
   has_many :users_favorite, through: :favorites, class_name: "User"
+  belongs_to :owner, class_name: "User"
+
+  validates :license_plate, presence: true, uniqueness: true
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :pickup_location, presence: true
+  validates :rental_instructions, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cars, only: [:index, :show]
+  resources :cars, only: [:index, :show, :new, :create]
   get "favorites/:user_id", to: "favorites#index"
   delete "favorites/:id", to: "favorites#destroy"
 end
