2b7d1291a32d2aaa964015fd448d745726348981
Chia Wei Ng <chiawei2410@gmail.com>
2021-05-29 13:33:42 +0800

    Created view booking outcome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def index
     @favorites = Favorite.where(user_id: params[:user_id])
     @car_reviews = CarReview.where(user_id: params[:user_id])
+    @confirmed = Booking.all.where("user_id = #{current_user.id} and bookings.status = 'accepted'")
+    @declined = Booking.all.where("user_id = #{current_user.id} and bookings.status = 'rejected'")
   end
 
 end
